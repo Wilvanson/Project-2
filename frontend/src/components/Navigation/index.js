6a246c7a36f80a5e80a7ae005098117710346365
wilvanson <wilvanson.dutervil@students.cookman.edu>
2022-02-05 18:08:16 -0500

    demo

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch} from 'react-redux';
+import * as sessionActions from '../../store/session';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+
+  const handleDemo = (e) => {
+    e.preventDefault();
+
+    return dispatch(sessionActions.login({ credential: 'demo@user.io', password: 'password'}))
+  }
 
   let sessionLinks;
   if (sessionUser) {
@@ -17,6 +25,7 @@ function Navigation({ isLoaded }){
       <>
         <NavLink to="/login">Log In</NavLink>
         <NavLink to="/signup">Sign Up</NavLink>
+        <button onClick={handleDemo}>Demo</button>
       </>
     );
   }
